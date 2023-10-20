93ab4e68e7f09eb01f313769d7f9d50afa364384
BlackStar1313 <pegpeg07@hotmail.com>
2023-10-20 22:07:43 +0200

    adding first files

---
+++ Master-C++/Standard-Template-Library(STL)/iterators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+  // Input iterator. Create an input iterator to read values from cin
+//  double value1, value2;
+//  std::cout << "Please, insert two values: ";
+
+//  std::istream_iterator<double> eos;              // end-of-stream iterator
+//  std::istream_iterator<double> iit (std::cin);   // stdin iterator
+
+//  if (iit != eos) value1=*iit;
+
+//  ++iit;
+
+//  if (iit != eos) value2=*iit;
+
+//  std::cout << value1 << "*" << value2 << "=" << (value1*value2) << '\n';
+
+  // Output iterator
+//  vector<int> myvector;
+//  for (int i=1; i<10; ++i) myvector.push_back(i*10);
+
+//  ostream_iterator<int> out_it (cout,", ");
+//  copy ( myvector.begin(), myvector.end(), out_it );
+
+  return 0;
+}
